d3c8c58f412d8e5e489c8783c1b82b675592788e
Marcel Arndt <marcel.arndt@hotmail.com>
2025-11-24 17:08:14 +0100

    soem styling

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavigationState } from './navigation-state';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { IconComponent } from '../icons/icon/icon';
 
 @Component({
   selector: 'app-navigation',
-  imports: [CommonModule, RouterLink,],
+  imports: [CommonModule, RouterLink, IconComponent],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss',
 })
