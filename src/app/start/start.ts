2482f73209022d8060e144d4731ebeab21b154db
Marcel Arndt <marcel.arndt@hotmail.com>
2025-11-26 10:29:54 +0100

    adding cards at homepage

--- src/app/start/start.ts
+++ src/app/start/start.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { GirdCards } from '../cards-section/gird-cards/gird-cards';
+import { Cards } from '../cards-section/cards/cards';
+import { IconComponent } from '../icons/icon/icon';
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [ GirdCards,Cards, IconComponent  ],
   templateUrl: './start.html',
   styleUrl: './start.scss',
 })
